04c71398a00653549c3b2d2b2991b3f64e358a80
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2025-10-06 11:18:09 +0300

    Add review part 1

--- review/Program.cs
+++ review/Program.cs
@@ -1,3 +1,36 @@
-﻿int a = 3;
+﻿// TASKS: Operations and variables
+int a = 3;
 int b = 7;
 Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+
+double c = 3.1415;
+Console.WriteLine(c + a);
+Console.WriteLine(c * a);
+
+string name = "pencho";
+Console.WriteLine(name);
+
+float d = 2.51f;
+
+// double + double
+Console.WriteLine(d + c);
+
+// float * int
+Console.WriteLine(d * 2);
+// float * (float)cast
+Console.WriteLine(d * (float)2);
+// float * float
+Console.WriteLine(d * 2f);
+
+// Translate sentence to C#:
+// I want to create a float variable with value 5.1234.
+float neshto = 5.1234f;
+
+// =============================================================================
+
+// Example:
+int number = 5;
+
+string text = Console.ReadLine();
+Console.WriteLine(text);

736504467b3ce80e083f8d95304e9ab7600205ac
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2025-10-08 01:32:09 +0300

    Add home work example

---
+++ homework-example/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input the amount of numbers that will follow:");
+string input = Console.ReadLine();
+int count = int.Parse(input);
+
+double sum = 0;
+for (int i = 0; i < count; ++i)
+{
+    input = Console.ReadLine();
+    double number = double.Parse(input);
+
+    sum += number;
+}
+
+Console.WriteLine("Average sum:");
+Console.WriteLine(sum / count);

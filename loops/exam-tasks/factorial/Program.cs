40e5af5a6900b6759dfc9123325891cb4566335b
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2025-11-11 23:17:58 +0200

    Add factorial solution

---
+++ loops/exam-tasks/factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿int num =int.Parse(Console.ReadLine());
+if (num < 0)
+{
+    Console.WriteLine("this number is negative");
+
+
+}
+else
+{
+    if (num == 0)
+    {
+        Console.WriteLine(1);
+    }
+    else
+    {
+        int product = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            product = product* i;
+        }
+        Console.WriteLine(product);
+    }
+}

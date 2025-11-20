02cd2c3e3b298f37e41288279ec05da0e8ce735f
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2025-11-20 23:33:10 +0200

    Add arrays-exercies

---
+++ arrays/arrays-exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[7];
+int max = int.MinValue;
+int min = int.MaxValue;
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    sum += array[i];
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine(sum);
+Console.WriteLine(max + " " + min);

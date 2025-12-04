fa9a272d15d752a4d405dd0f53ac21cdb1f316be
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2025-12-04 13:56:33 +0200

    Array exam prep part 1

---
+++ arrays/exam-prep/Program.cs
@@ -0,0 +1,20 @@
+﻿int n  = int.Parse(Console.ReadLine());
+int[] Array = new int[n];
+int Max = int.MinValue;
+int sum = 0;
+int negative = 0;
+for (int i = 0; i < n; i++)
+{
+    Array[i] = int.Parse(Console.ReadLine());
+    if (Array[i] > 0)
+    {
+        sum = sum + Array[i];
+    }
+    else
+    {
+        negative = negative + Array[i];
+    }
+}
+Console.WriteLine(sum);
+Console.WriteLine(negative);
+Console.WriteLine(sum + negative / n);

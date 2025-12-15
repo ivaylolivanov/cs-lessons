624d6c69665872b3e2388492fc04b5266a84d4e2
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2025-12-15 11:51:12 +0200

    Add left shift and only odds tasks' solutions

---
+++ arrays/exam-solutions/task-left-shift/Program.cs
@@ -0,0 +1,41 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = int.Parse(Console.ReadLine());
+}
+
+// Solution 1:
+/*
+int firstNumber  = array[0];
+int secondNumber = array[1];
+for (int i = 2; i < array.Length; i++)
+{
+    array[i - 2] = array[i];
+}
+
+array[array.Length - 2] = firstNumber;
+array[array.Length - 1] = secondNumber;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+*/
+
+// Solution 2:
+for (int repetition = 0; repetition < 2; repetition++)
+{
+    int firstNumber = array[0];
+    int secondNumber = array[1];
+    for (int i = 1; i < array.Length; i++)
+    {
+        array[i - 1] = array[i];
+    }
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
---
+++ arrays/exam-solutions/task-only-odds/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+bool hasOdds = false;
+
+for (int i = 0; i < n; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+    if ((array[i] % 2) != 0)
+    {
+        hasOdds = true;
+        Console.WriteLine(array[i]);
+    }
+}
+
+if (!hasOdds)
+{
+    Console.WriteLine("No Odds!");
+}

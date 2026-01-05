81d3116e89186807f4eb369a22effa83a4344f38
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-01-05 11:40:11 +0200

    Add movie present and self information

---
+++ first-term-review/variables/movie-present/Program.cs
@@ -0,0 +1,14 @@
+﻿string movieName = "The Mask";
+float hours = 1.5f;
+int year = 1994;
+string genre = "Comedy";
+string regisseur = "Chuck Russell";
+
+// Alternative:
+Console.WriteLine($"title: {movieName}");
+
+Console.WriteLine("Title: " + movieName);
+Console.WriteLine("Hours: " + hours);
+Console.WriteLine("Year: " + year);
+Console.WriteLine("Genre: " + genre);
+Console.WriteLine("regisseur: " + regisseur);
---
+++ first-term-review/variables/self-intro/Program.cs
@@ -0,0 +1,11 @@
+﻿string name = "Ivaylo";
+int year = 1995;
+int month = 13;
+int day = 32;
+string favoriteColor = "black";
+string zodiac = "leo";
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Birth date: " + day + " / " + month + " / " + year);
+Console.WriteLine("Favorite color: " + favoriteColor);
+Console.WriteLine("Zodiac: " + zodiac);

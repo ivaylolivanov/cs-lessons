a2cb8bdaf460bac4fa89f6e38eaaf9dbab41dade
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-01-09 15:05:50 +0200

    Add simple operations
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

---
+++ first-term-review/simple-operations/Program.cs
@@ -0,0 +1,12 @@
+﻿string variable = "5";
+double moleculeMass = 0.123415;
+int number = 2;
+int number2 = 10;
+float temperature = -5.2f;
+
+float sum = number + temperature;
+Console.WriteLine(sum);
+double multiplication = number * temperature;
+Console.WriteLine(multiplication);
+multiplication = number2 * temperature;
+Console.WriteLine(multiplication);

a245bcf1b1b95480cbe6c5b4ed131164b29b6b2e
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-01-20 12:38:23 +0200

    Upload Conditionals

---
+++ first-term-review/conditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter a hour:");
+int hour = int.Parse( Console.ReadLine());
+
+if (hour >= 6 && hour <= 10)
+{
+    Console.WriteLine("Good morning");
+}
+else if (hour >= 11 && hour <= 18)
+{
+    Console.WriteLine("Good afternoon");
+}
+else if (hour >= 19 && hour <= 22)
+{
+    Console.WriteLine("Good evening");
+}
+else if (hour >= 23 && hour <= 24 || hour == 0)
+{
+    Console.WriteLine("Good night");
+}
+else if (hour >= 1 && hour <= 5)
+{
+    Console.WriteLine("Was it a good party?");
+}
+else
+{
+    Console.WriteLine("Not a valid hour");
+}

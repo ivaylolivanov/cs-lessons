037e6ffbe558b73f8b265d92c7baf49c6e35aed1
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-02-04 11:17:21 +0200

    Add loop tasks

---
+++ first-term-review/loops/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Numbers from 0 100, including.
+ * Print each 10 numbers on new line.
+ *
+ * Example:
+ * 0  1  2  3  4  5  6  7  8  9
+ * 10 11 12 13 14 15 16 17 18 19
+ *              ...
+ * 100
+ */
+
+for (int i = 0; i <= 100; i++)
+{
+    Console.Write(i + " ");
+
+    if (i % 10 == 9)
+    {
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();
+
+int num = 0;
+while (num <= 100)
+{
+    Console.Write(num + " ");
+
+    if (num % 10 == 9)
+    {
+        Console.WriteLine();
+    }
+
+    num++;
+}
+Console.WriteLine();

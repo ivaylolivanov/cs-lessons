f28eea1fff8ceeedfe215f3b038857ec6fb7582d
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-02-12 06:02:38 +0200

    Add more projects in loops review, add new task solution

---
+++ first-term-review/loops/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿int number = 0;
+int negative = 0;
+
+do
+{
+    number = int.Parse(Console.ReadLine());
+    if (number < 0)
+    {
+        int digits = 0;
+        int numberCopy = number;
+        while (numberCopy != 0)
+        {
+            numberCopy = numberCopy / 10;
+            digits++;
+        }
+
+        if ((digits % 2) == 0)
+        {
+            negative = number;
+            break;
+        }
+    }
+
+} while (number != 0);
+
+if (negative == 0)
+{
+    Console.WriteLine("No suitable number found!");
+}
+else
+{
+    Console.WriteLine(negative);
+}

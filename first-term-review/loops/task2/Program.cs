e3eb66ecb9ff20426da3dca40d7beaf31f079c09
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-02-16 09:49:39 +0200

    Add task2

---
+++ first-term-review/loops/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = 0;
+int lastNumber = number;
+int diff = 0;
+int length = 0;
+
+do
+{
+    number = int.Parse(Console.ReadLine());
+
+    diff = number - lastNumber;
+    if ((diff == 1) || (diff == number))
+    {
+        length++;
+    }
+    else
+    {
+    }
+
+    lastNumber = number;
+} while (number != 0);

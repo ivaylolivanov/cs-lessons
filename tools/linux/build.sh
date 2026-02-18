016b7e61e7bdef166a5ca221ea7f8cc86d8a2965
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-02-18 12:40:04 +0200

    build.sh: Now includes the code examples in the list
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- tools/linux/build.sh
+++ tools/linux/build.sh
@@ -4,11 +4,26 @@ REPO_ROOT="$(git rev-parse --show-toplevel)";
 OUTPUT_DIR="${REPO_ROOT}/docs/data";
 OUTPUT_FILE="${OUTPUT_DIR}/readme-paths.txt";
 
+function generate_files_list()
+{
+    local file_readme='';
+    local file_cs='';
+    local directory='';
+
+    local cs_files=();
+    local readme_files=( $(IFS=$'\n'; find "${REPO_ROOT}" -type f -iname "readme.md";) );
+
+    for file_readme in "${readme_files[@]}"; do
+        directory="$(dirname "${file_readme}")";
+        cs_files=( $(IFS=$'\n'; find "${directory}" -type f -iname "Program.cs") );
+
+        echo "${file_readme}";
+        for file_cs in "${cs_files[@]}"; do
+            echo "${file_cs#${REPO_ROOT}/}";
+        done
+    done
+}
+
 mkdir -pv "$OUTPUT_DIR";
-
-find "${REPO_ROOT}" -type f -iname "readme.md" -print0 \
-    | while IFS= read -r -d '' file; do
-        echo -e "${file#$REPO_ROOT/}";
-done > "$OUTPUT_FILE";
-
+generate_files_list > "${OUTPUT_FILE}" || return 1;
 echo "${OUTPUT_FILE} generated successfully.";

dc9c80c2ce4c41b92aaa803a85e747386cb7b6b5
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-02-23 12:55:23 +0200

    Solve temperature task
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- first-term-review/arrays/task1/Program.cs
+++ first-term-review/arrays/task1/Program.cs
@@ -1,15 +1,34 @@
 ﻿int minValue = int.MaxValue;
+int maxValue = int.MinValue;
 
-short[] temperatures = new short[30];
-
+short[] temperatures = new short[5];
+double average = 0;
 for (int i = 0; i < temperatures.Length; i++)
 {
     temperatures[i] = short.Parse(Console.ReadLine());
+    average += temperatures[i];
     if (temperatures[i] < minValue)
     {
         minValue = temperatures[i];
     }
+
+    if (temperatures[i] > maxValue)
+    {
+        maxValue = temperatures[i];
+    }
 }
 
-Console.WriteLine($"The lowest temperature: {minValue}");
-Console.WriteLine(minValue);
+average = Math.Round(average / temperatures.Length, 2);
+short count = 0;
+for (int i = 0; i < temperatures.Length; i++)
+{
+    if (temperatures[i] < average)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"The lowest temperature:     {minValue}");
+Console.WriteLine($"The highest temperature:    {maxValue}");
+Console.WriteLine($"The average temperature:    {average}");
+Console.WriteLine($"Temperatures below average: {count}");

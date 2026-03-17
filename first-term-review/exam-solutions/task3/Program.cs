873275c935ff7c09c3cb15e2bb5f3813d52eceb5
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-03-17 13:11:26 +0200

    Add task3 solution

---
+++ first-term-review/exam-solutions/task3/Program.cs
@@ -0,0 +1,35 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] numbers = new int[n];
+float average = 0;
+for (int i = 0; i < n; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+    average += numbers[i];
+}
+
+int count = 0;
+average /= n;
+for (int i = 0; i < n; i++)
+{
+    bool isBigger = numbers[i] >= average;
+    if (isBigger)
+    {
+        int duplicates = 0;
+        for (int j = 0; j < n; j++)
+        {
+            bool isDifferent = i != j;
+            bool isDup = numbers[i] == numbers[j];
+            if (isDifferent && isDup)
+            {
+                duplicates++;
+            }
+        }
+
+        if (duplicates > 0)
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine(count);

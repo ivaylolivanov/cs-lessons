ca203e1ac6e8309307e086264b2305730e3a4468
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-03-27 12:09:43 +0200

    Add Functions examples
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

---
+++ functions/introduction/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintNumbers(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+bool IsEven(int number)
+{
+    bool result = (number % 2) == 0;
+    return result;
+}
+
+int[] ReadNumbers(int size)
+{
+    int[] numbers = new int[size];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = int.Parse(Console.ReadLine());
+    }
+
+    return numbers;
+}
+
+Console.WriteLine("Function:");
+PrintNumbers(27, 38);
+Console.WriteLine(
+    "Is 7 an even number: " + IsEven(7));
+
+int[] myNumbers = ReadNumbers(7);
+Console.WriteLine(myNumbers[2]);
